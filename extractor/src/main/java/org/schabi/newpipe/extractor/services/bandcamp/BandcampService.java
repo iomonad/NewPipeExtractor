9e4e20e40a5ddc2cf3afe1442a8cfe02d5afe4be
Clement T <me@trosa.io>
2019-09-18 17:13:27 +0200

    Added Bandcamp support boilerplate

---
+++ extractor/src/main/java/org/schabi/newpipe/extractor/services/bandcamp/BandcampService.java
@@ -0,0 +1,94 @@
+package org.schabi.newpipe.extractor.services.bandcamp;
+
+import org.schabi.newpipe.extractor.StreamingService;
+import org.schabi.newpipe.extractor.SuggestionExtractor;
+import org.schabi.newpipe.extractor.channel.ChannelExtractor;
+import org.schabi.newpipe.extractor.comments.CommentsExtractor;
+import org.schabi.newpipe.extractor.exceptions.ExtractionException;
+import org.schabi.newpipe.extractor.kiosk.KioskList;
+import org.schabi.newpipe.extractor.linkhandler.LinkHandler;
+import org.schabi.newpipe.extractor.linkhandler.LinkHandlerFactory;
+import org.schabi.newpipe.extractor.linkhandler.ListLinkHandler;
+import org.schabi.newpipe.extractor.linkhandler.ListLinkHandlerFactory;
+import org.schabi.newpipe.extractor.linkhandler.SearchQueryHandler;
+import org.schabi.newpipe.extractor.linkhandler.SearchQueryHandlerFactory;
+import org.schabi.newpipe.extractor.playlist.PlaylistExtractor;
+import org.schabi.newpipe.extractor.search.SearchExtractor;
+import org.schabi.newpipe.extractor.stream.StreamExtractor;
+import org.schabi.newpipe.extractor.subscription.SubscriptionExtractor;
+import org.schabi.newpipe.extractor.utils.Localization;
+
+import static java.util.Collections.singletonList;
+import static org.schabi.newpipe.extractor.StreamingService.ServiceInfo.MediaCapability.AUDIO;
+
+public class BandcampService extends StreamingService {
+
+    public BandcampService(int id) {
+        super(id, "Bandcamp", singletonList(AUDIO));
+    }
+
+    @Override
+    public LinkHandlerFactory getStreamLHFactory() {
+        return null;
+    }
+
+    @Override
+    public ListLinkHandlerFactory getChannelLHFactory() {
+        return null;
+    }
+
+    @Override
+    public ListLinkHandlerFactory getPlaylistLHFactory() {
+        return null;
+    }
+
+    @Override
+    public SearchQueryHandlerFactory getSearchQHFactory() {
+        return null;
+    }
+
+    @Override
+    public ListLinkHandlerFactory getCommentsLHFactory() {
+        return null;
+    }
+
+    @Override
+    public SearchExtractor getSearchExtractor(SearchQueryHandler queryHandler, Localization localization) {
+        return null;
+    }
+
+    @Override
+    public SuggestionExtractor getSuggestionExtractor(Localization localization) {
+        return null;
+    }
+
+    @Override
+    public SubscriptionExtractor getSubscriptionExtractor() {
+        return null;
+    }
+
+    @Override
+    public KioskList getKioskList() throws ExtractionException {
+        return null;
+    }
+
+    @Override
+    public ChannelExtractor getChannelExtractor(ListLinkHandler linkHandler, Localization localization) throws ExtractionException {
+        return null;
+    }
+
+    @Override
+    public PlaylistExtractor getPlaylistExtractor(ListLinkHandler linkHandler, Localization localization) throws ExtractionException {
+        return null;
+    }
+
+    @Override
+    public StreamExtractor getStreamExtractor(LinkHandler linkHandler, Localization localization) throws ExtractionException {
+        return null;
+    }
+
+    @Override
+    public CommentsExtractor getCommentsExtractor(ListLinkHandler linkHandler, Localization localization) throws ExtractionException {
+        return null;
+    }
+}
